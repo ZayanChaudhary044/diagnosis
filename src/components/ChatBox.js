1b0861965b3a3b3ec62dd115d6e403f928224e49
Zayan Chaudhary <ec23095@qmul.ac.uk>
2025-08-14 13:39:15 +0100

    Made a start on chatbox

---
+++ src/components/ChatBox.js
@@ -0,0 +1,11 @@
+
+const ChatBox = () => {
+    return(
+        <div>
+            <h1>Your Virtual AI Assistant</h1>
+            <span className="material-symbols-outlined">keyboard_arrow_down</span>
+        </div>
+    )
+};
+
+export default ChatBox
--- src/pages/SymptomCheck.js
+++ src/pages/SymptomCheck.js
@@ -1,11 +1,13 @@
 // src/pages/SymptomCheck.js
 import React from 'react';
+import ChatBox from '../components/ChatBox.js';
 
 function SymptomCheck() {
   return (
     <div>
       <h2>Symptom Checker</h2>
       <p style={{ marginLeft: '20px'}}>Enter your symptoms to begin diagnosis.</p>
+      <ChatBox />
     </div>
   );
 }
